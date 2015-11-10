04377e7a1b388eedb4d25fddde78aabe1c1a7f9f
Matthew Allen Lin <Darthpwner@s-169-232-70-217.resnet.ucla.edu>
2015-11-09 22:30:33 -0800

    Method to parse input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,23 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Receipt r = new Receipt();
-        System.out.println(r.hasImportTax("imported hello"));
-        System.out.println(r.getPrice("Fuck boy 19.81"));
+//        System.out.println(r.hasImportTax("imported hello"));
+//        System.out.println(r.getPrice("Fuck boy 19.81" +
+//                "Hello there"));
+        Main m = new Main();
+
+        m.getOutput("1 book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85");
     }
 
     void getOutput(String input) {
         Receipt r = new Receipt();
 
-        r.addLineToReceipt(input);
+        String text[] = r.parseText(input);
+        for(int i = 0; i < text.length; i++) {
+            System.out.println(text[i]);
+        }
+
+        //r.addLineToReceipt(input);
     }
 
 }
