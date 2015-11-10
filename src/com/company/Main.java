e81af2b2f7b16b702d7dbd3b947d8960e6a63bf0
Matthew Allen Lin <Darthpwner@s-169-232-70-217.resnet.ucla.edu>
2015-11-09 23:29:35 -0800

    Fix add tax functiong

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,6 +25,7 @@ public class Main {
         for(int i = 0; i < r.getTextLength(); i++) {
             print[i] = r.getText(i).replaceAll("\\bat\\b", ":"); //Regex to find only exact match for "at"
             r.addCost(r.getPrice(print[i]));
+            r.addTax(r.getTax(r.getText(i), i));    //Fix this shit
 
             System.out.println(print[i]);
         }
