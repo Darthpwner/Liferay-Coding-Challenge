5ffdde7679b113d1c8f80b5d412af0a3445d3099
Matthew Allen Lin <Darthpwner@s-169-232-70-217.resnet.ucla.edu>
2015-11-10 01:37:47 -0800

    Submit this to Liferay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,22 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Receipt r = new Receipt();
-//        System.out.println(r.hasImportTax("imported hello"));
-//        System.out.println(r.getPrice("Fuck boy 19.81" +
-//                "Hello there"));
         Main m = new Main();
 
-        m.getOutput("2 book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85");
+        m.getOutput("1 book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85");
         m.getOutput("1 imported book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85");
         m.getOutput("1 imported box of chocolates at 10.00\n1 imported bottle of perfume at 47.50");
         m.getOutput("1 imported bottle of perfume at 27.99\n1 bottle of perfume at 18.99\n1 packet of headache pills at 9.75\n1 imported box of chocolates at 11.25");
+    }
+
+    String padZeros(String amount) {
+        String x = "" + amount.charAt(amount.length() - 2);
+
+        if(x.equals(".")) {
+            return amount + "0";
+        }
+
+        return amount;
     }
 
     void getOutput(String input) {
@@ -33,15 +40,20 @@ public class Main {
             double totalIndividualPrice = r.getPrice(print[i]) + r.getTax(r.getText(i), r.getPrice(print[i]));
             totalIndividualPrice = Math.round(totalIndividualPrice * 100.0)/100.0;
             String totalIndividualPriceString = String.valueOf(totalIndividualPrice);
+            totalIndividualPriceString = padZeros(totalIndividualPriceString);
             print[i] = r.getText(i).replaceAll(originalCost[originalCost.length-1], totalIndividualPriceString);
 
             System.out.println(print[i]);
         }
 
-        System.out.println("Sales Taxes : " + r.getTotalTax());
+        String taxString = String.valueOf(r.getTotalTax());
+        taxString = padZeros(taxString);
+        System.out.println("Sales Taxes : " + taxString);
         double total = r.getTotalCost() + r.getTotalTax();
         total = Math.round(total * 100.0)/ 100.0;   //Rounds to two decimal places
-        System.out.println("Total : " + total);
+        String totalString = String.valueOf(total);
+        totalString = padZeros(totalString);
+        System.out.println("Total : " + totalString);
 
         System.out.println("");
     }
